eaf1ed4d87735f3b7ade1cfeaf82e410211653d9
petri1807 <petri.lindholm1986@gmail.com>
2021-04-28 11:52:39 +0300

    6.3

---
+++ osa6/redux-anecdotes/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+    'react/prop-types': 0,
+    'no-unused-vars': 1,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
