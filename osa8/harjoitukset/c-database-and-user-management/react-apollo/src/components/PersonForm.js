4608ab8acb613dbc099a422d229d8b61266b8d90
petri.lindholm <petri.lindholm@woolman.io>
2022-06-02 14:52:57 +0300

    Välimuistin päivitys

--- osa8/harjoitukset/c-database-and-user-management/react-apollo/src/components/PersonForm.js
+++ osa8/harjoitukset/c-database-and-user-management/react-apollo/src/components/PersonForm.js
@@ -11,16 +11,30 @@ const PersonForm = ({ setError }) => {
   const [city, setCity] = useState('');
 
   const [createPerson] = useMutation(CREATE_PERSON, {
-    refetchQueries: [{ query: ALL_PERSONS }],
+    // refetchQueries: [{ query: ALL_PERSONS }],
     onError: (error) => {
-      setError(error.graphQLErrors[0].message);
+      setError(error.graphQLErrors[0]?.message);
+    },
+    update: (cache, response) => {
+      cache.updateQuery({ query: ALL_PERSONS }, ({ allPersons }) => {
+        return {
+          allPersons: allPersons.concat(response.data.addPerson),
+        };
+      });
     },
   });
 
   const submit = async (event) => {
     event.preventDefault();
 
-    createPerson({ variables: { name, phone, street, city } });
+    createPerson({
+      variables: {
+        name,
+        street,
+        city,
+        phone: phone.length > 0 ? phone : undefined,
+      },
+    });
 
     setName('');
     setPhone('');
